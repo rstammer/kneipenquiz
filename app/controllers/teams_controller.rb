8001bf9592482f9e15d3f75c6e96bfad116ac21f
Robin Neumann <robin.neumann@absolventa.de>
2014-03-13 23:01:37 +0100

    add team-crud stuff

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,14 @@
 class TeamsController < ActionController::Base
   inherit_resources
   layout 'application'
+
+  def create
+    build_resource
+    resource.save
+    redirect_to teams_path
+  end
+
+  def permitted_params
+    params.permit(team: [:title])
+  end
 end
