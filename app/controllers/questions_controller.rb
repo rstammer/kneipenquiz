571c0840c1005694e8962dc4c90fd859f8e37b94
Robin Neumann <robin.neumann@absolventa.de>
2014-03-14 20:52:33 +0100

    some CRUD basics for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,14 @@
 class QuestionsController < ActionController::Base
+  inherit_resources
   layout 'application'
 
-  def index
+  def create
+    build_resource
+    resource.save
+    redirect_to questions_path
   end
 
+  def permitted_params
+    params.permit(question: [:content, :answer])
+  end
 end
