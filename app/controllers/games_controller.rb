468cc6202a5251de1efa0b35793ae4edc5748de4
Robin Neumann <robin.neumann@absolventa.de>
2014-03-16 20:31:02 +0100

    prepare structure for entire game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,24 @@
 class GamesController < ActionController::Base
   layout 'application'
 
+  def load_game
+  end
+
+  def round_one
+  end
+
+  def round_two
+  end
+
+  def intermezzo
+  end
+
+  def round_three
+  end
+
+  def round_four
+  end
+
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Kneipenquiz::Application.routes.draw do
   root to: 'games#index'
 
-  resources :games
+  resources :games do
+    get :load_game, on: :collection
+    get :round_one, on: :member
+    get :round_two, on: :member
+    get :intermezzo, on: :member
+    get :round_three, on: :member
+    get :round_four, on: :member
+  end
+
   resources :questions
   resources :teams
 end
