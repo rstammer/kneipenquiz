a108ed0d93fd4617b78ba7546920ef72307b450f
Robin Neumann <robin.neumann@absolventa.de>
2014-03-16 10:25:13 +0100

    improve teams table and form

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,10 +2,21 @@ class TeamsController < ActionController::Base
   inherit_resources
   layout 'application'
 
+  def new
+    collection
+    build_resource
+  end
+
   def create
     build_resource
     resource.save
     redirect_to teams_path
+  end
+
+  private
+
+  def collection
+    @teams ||= Team.all
   end
 
   def permitted_params
