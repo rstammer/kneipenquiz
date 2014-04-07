0ecc3f9a0764e69767129c09301635ffc241b1da
Robin Neumann <robin.neumann@absolventa.de>
2014-04-07 21:18:32 +0200

    add spec skeleton for category_mappings

--- spec/models/category_mapping_spec.rb
+++ spec/models/category_mapping_spec.rb
@@ -1,10 +1,21 @@
 require 'spec_helper'
 
-describe CategoryMappings do
+describe CategoryMapping do
+
+  let(:category) { FactoryGirl.create(:category) }
+  let(:question) { FactoryGirl.create(:question) }
 
   context 'with associations' do
     it { expect(subject).to belong_to :question }
     it { expect(subject).to belong_to :category }
+  end
+
+  context 'with class methods' do
+    describe '#map' do
+      it 'returns existing mapping' do
+
+      end
+    end
   end
 
 end
