69c34da2bd32b0ab43fdd050f38c8af722965561
Robin Neumann <robin.neumann@absolventa.de>
2014-04-17 22:17:21 +0200

    drop all the paperclip-dropbox shizzle

--- app/models/question.rb
+++ app/models/question.rb
@@ -33,19 +33,7 @@ class Question < ActiveRecord::Base
   #
 
   acts_as_taggable
-  has_attached_file :image,
-    :styles => { :large => "800x800>", :medium => "300x300>", :thumb => "100x100>" },
-    :storage => :dropbox,
-    #:dropbox_credentials => Rails.root.join("config/dropbox.yml")
-
-    :dropbox_credentials => {
-      app_key:             ENV['DROPBOX_APP_KEY'],
-      app_secret:          ENV['DROPBOX_APP_SECRET'],
-      access_token:        ENV['DROPBOX_ACCESS_TOKEN'],
-      access_token_secret: ENV['DROPBOX_ACCESS_TOKEN_SECRET'],
-      user_id:             ENV['DROPBOX_USER_ID'],
-      access_type:         "app_folder"
-    }
+  has_attached_file :image, :styles => { :large => "800x800>", :medium => "300x300>", :thumb => "100x100>" }
 
   # validations
   #
