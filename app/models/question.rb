0ea56d226c52f4fba5f8899e240abf9af143c7e2
Robin Neumann <robin.neumann@absolventa.de>
2014-04-18 13:34:10 +0200

    try s3 storage for paperclip

--- app/models/question.rb
+++ app/models/question.rb
@@ -33,7 +33,8 @@ class Question < ActiveRecord::Base
   #
 
   acts_as_taggable
-  has_attached_file :image, :styles => { :large => "800x800>", :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image,
+    styles: { large: "800x800>", medium: "300x300>", thumb: "100x100>" }
 
   # validations
   #
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,15 @@ Kneipenquiz::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 
   # devise
-  config.action_mailer.default_url_options = { host: 'zuz-kneipenquiz.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'kneipenquiz.herokuapp.com' }
 end
