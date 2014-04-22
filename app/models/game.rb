0050739fd09a788a0b8b775733c692a09f2ad5cc
Robin Neumann <robin.neumann@absolventa.de>
2014-04-22 21:43:08 +0200

    fix form input

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,11 +1,19 @@
 class Game < ActiveRecord::Base
 
+  # associations
+  #
+  #
+
   has_many :results
   has_many :questions
 
+  # instance methods
+  #
+  #
 
   def question(round, number)
     Question.where(round: round, game_id: self.id).first
   end
+
 end
 
