bd5da8d0fc9d49a1f5111e7bfce81208dd9c96e6
Robin Neumann <robin.neumann@absolventa.de>
2014-04-24 09:24:31 +0200

    drop fishy method

--- app/models/game.rb
+++ app/models/game.rb
@@ -7,13 +7,5 @@ class Game < ActiveRecord::Base
   has_many :results
   has_many :questions
 
-  # instance methods
-  #
-  #
-
-  def question(round, number)
-    Question.where(round: round, game_id: self.id).first
-  end
-
 end
 
