1d33cc94b5b4957dfec69f1030d854bcf1ffbe38
Robin Neumann <robin.neumann@absolventa.de>
2014-04-28 21:37:09 +0200

    improve filtering

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,11 +6,15 @@ module ApplicationHelper
   end
 
   def options_for_typus
-    Question::TYPES.map { |type| [type, type] } + [['alle', nil]]
+    [['alle Typen', nil]] + Question::TYPES.map { |type| [type, type] }
   end
 
   def options_for_questions
     [['auswählen', nil]] + Question.all.map { |q| [q.title, q.id] }
+  end
+
+  def options_for_cats
+    [['alle Kategorien', nil]] + Question::CATEGORIES.keys.map { |cat| [cat, cat] }
   end
 
   def selected(game, round, position)
