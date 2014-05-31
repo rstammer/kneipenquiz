4b8324d70fdc2a5c732eae01e9db787cc1397532
Robin Neumann <robin.neumann@absolventa.de>
2014-05-31 13:20:30 +0200

    rename js module

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require fullscreen_control
+//= require fullscreen_handler
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
--- app/assets/javascripts/fullscreen_handler.js
+++ app/assets/javascripts/fullscreen_handler.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var FullscreenControl = (function(){
+var FullscreenHandler = (function(){
 
   var control = {};
 
