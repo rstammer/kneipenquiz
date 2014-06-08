34137ec8ff6c2699fa31cc721b67577769970d94
Robin Neumann <robin.neumann@absolventa.de>
2014-06-08 22:23:20 +0200

    sadly say goodbye to slider_rider!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require absolventa/slide_rider
 //= require fullscreen_handler
 //= require jquery
 //= require jquery_ujs
