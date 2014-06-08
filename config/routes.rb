d1f1283dda48d53405e6cbdb9c1e03c3f1845493
Robin Neumann <robin.neumann@absolventa.de>
2014-06-08 22:30:48 +0200

    drop crazy routing structure

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,7 @@ Kneipenquiz::Application.routes.draw do
   root to: 'games#index'
 
   resources :games do
-    get :load_game,   on: :collection
-    get :round_one,   on: :member
-    get :round_two,   on: :member
-    get :intermezzo,  on: :member
-    get :round_three, on: :member
-    get :round_four,  on: :member
+    get :load_game, on: :collection
   end
 
   resources :category_mappings
