c5d2687741bee416b631d8495820dd65f6a11a7d
Robin Neumann <robin.neumann@absolventa.de>
2014-11-11 21:22:11 +0100

    add comments and fallbacks

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,7 +1,15 @@
 class Result < ActiveRecord::Base
 
+  # associations
+  #
+  #
+
   belongs_to :team
   belongs_to :game
+
+  # validations
+  #
+  #
 
   validates :round_one,
             :round_two,
@@ -12,7 +20,15 @@ class Result < ActiveRecord::Base
             :game_id,
             presence: true
 
+  # callbacks
+  #
+  #
+
   after_create :update_team_score
+
+  # instance methods
+  #
+  #
 
   def update_team_score
     new_value = team.total_score.to_f + self.total_score.to_f
