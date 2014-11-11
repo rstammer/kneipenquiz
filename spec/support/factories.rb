56d01c88fbfaec9810805974a936e21086c2b358
Robin Neumann <robin.neumann@absolventa.de>
2014-11-11 19:34:54 +0100

    define useful factories

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -21,4 +21,18 @@ FactoryGirl.define do
     password '1234565767503242'
   end
 
+  factory :result do
+    round_one 1
+    round_two 2
+    round_three 3
+    round_four 4
+    total_score 10
+    team
+    game
+  end
+
+  factory :team do
+    title 'SV Werder'
+  end
+
 end
