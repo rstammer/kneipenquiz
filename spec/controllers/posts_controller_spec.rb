643ac233191cf6a06c305698e9d4cc3aa74f714c
Robin Neumann <robin.neumann@absolventa.de>
2014-12-20 13:49:40 +0100

    Fix specs

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,6 +2,14 @@ require 'spec_helper'
 
 describe PostsController do
 
+  include Devise::TestHelpers
+
+  before(:each) do
+    controller.request.env["devise.mapping"] = Devise.mappings[:user]
+    @user = User.first || FactoryGirl.create(:user)
+    sign_in @user
+  end
+
   describe 'GET index' do
     it 'renders index' do
       get :index
